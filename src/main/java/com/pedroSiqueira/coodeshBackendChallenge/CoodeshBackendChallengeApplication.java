79924a6a35f9328de076e554f257ecc66f6ca2d2
Pedro Siqueira <pedro.hsmf@live.com>
2021-03-09 21:37:13 -0300

    Created Base Spring Project

---
+++ src/main/java/com/pedroSiqueira/coodeshBackendChallenge/CoodeshBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pedroSiqueira.coodeshBackendChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoodeshBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoodeshBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroSiqueira/coodeshBackendChallenge/CoodeshBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroSiqueira.coodeshBackendChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoodeshBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
